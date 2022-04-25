5b709c5bdc74584386b721a97051393f2ea2aa74
crumpl07 <crumpst3r2000@gmail.com>
2022-04-24 21:57:57 -0400

    Updated code base

--- code/main.py
+++ code/main.py
@@ -1,5 +1,3 @@
-# I lov emy girlfiredn vivian
-
 import torch
 import torch.cuda
 from torch.utils.data import DataLoader
@@ -7,6 +5,11 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as T
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
+from torchvision.utils import save_image
+import torch.nn as nn
+from tqdm import tqdm
+from IPython.display import Image
+import torch.nn.functional as F
 import os
 
 def denorm(img_tensors):
@@ -16,34 +19,12 @@ def show_images(images, nmax=64):
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.set_xticks([]); ax.set_yticks([])
     ax.imshow(make_grid(denorm(images.detach()[:nmax]), nrow=8).permute(1, 2, 0))
-    plt.show()
+    plt.savefig("Cats")
 
 def show_batch(dl, nmax=64):
     for images, _ in dl:
         show_images(images, nmax)
         break
-
-project_name = 'CS455_PROJECT'
-DATA_DIR = '../afhq/'
-
-image_size = 64
-batch_size = 128
-stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
-
-print(os.listdir(DATA_DIR)[:10])
-print(torch.cuda.is_available())
-
-# print(DATA_DIR)
-
-train_ds = ImageFolder(DATA_DIR, transform=T.Compose([
-    T.Resize(image_size),
-    T.CenterCrop(image_size),
-    T.ToTensor(),
-    T.Normalize(*stats)]))
-
-train_dl = DataLoader(train_ds, batch_size, shuffle=True, pin_memory=True)
-
-show_batch(train_dl)
 
 def get_default_device():
     """Pick GPU if available, else CPU"""
@@ -51,7 +32,7 @@ def get_default_device():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
-    
+
 def to_device(data, device):
     """Move tensor(s) to chosen device"""
     if isinstance(data, (list,tuple)):
@@ -73,13 +54,121 @@ class DeviceDataLoader():
         """Number of batches"""
         return len(self.dl)
 
+def train_discriminator(real_images, opt_d):
+    # Clear discriminator gradients
+    opt_d.zero_grad()
+
+
+    # Pass real images through discriminator
+    real_preds = discriminator(real_images)
+    real_targets = torch.ones(real_images.size(0), 1, device=device)
+    real_loss = F.binary_cross_entropy(real_preds, real_targets)
+    real_score = torch.mean(real_preds).item()
+    
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
+    fake_images = generator(latent)
+
+    # Pass fake images through discriminator
+    fake_targets = torch.zeros(fake_images.size(0), 1, device=device)
+    fake_preds = discriminator(fake_images)
+    fake_loss = F.binary_cross_entropy(fake_preds, fake_targets)
+    fake_score = torch.mean(fake_preds).item()
+
+    # Update discriminator weights
+    loss = real_loss + fake_loss
+    loss.backward()
+    opt_d.step()
+    return loss.item(), real_score, fake_score
+
+def train_generator(opt_g):
+    # Clear generator gradients
+    opt_g.zero_grad()
+    
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
+    fake_images = generator(latent)
+    
+    # Try to fool the discriminator
+    preds = discriminator(fake_images)
+    targets = torch.ones(batch_size, 1, device=device)
+    loss = F.binary_cross_entropy(preds, targets)
+    
+    # Update generator weights
+    loss.backward()
+    opt_g.step()
+    
+    return loss.item()
+
+def save_samples(index, latent_tensors, show=True):
+    fake_images = generator(latent_tensors)
+    fake_fname = 'generated-images-{0:0=4d}.png'.format(index)
+    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=8)
+    print('Saving', fake_fname)
+    if show:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
+
+def fit(epochs, lr, start_idx=1):
+    torch.cuda.empty_cache()
+    
+    # Losses & scores
+    losses_g = []
+    losses_d = []
+    real_scores = []
+    fake_scores = []
+    
+    # Create optimizers
+    opt_d = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+    opt_g = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
+    
+    for epoch in range(epochs):
+        for real_images, _ in tqdm(train_dl):
+            # Train discriminator
+            loss_d, real_score, fake_score = train_discriminator(real_images, opt_d)
+            # Train generator
+            loss_g = train_generator(opt_g)
+            
+        # Record losses & scores
+        losses_g.append(loss_g)
+        losses_d.append(loss_d)
+        real_scores.append(real_score)
+        fake_scores.append(fake_score)
+        
+        # Log losses & scores (last batch)
+        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
+            epoch+1, epochs, loss_g, loss_d, real_score, fake_score))
+    
+        # Save generated images
+        save_samples(epoch+start_idx, fixed_latent, show=False)
+    
+    return losses_g, losses_d, real_scores, fake_scores
+
+project_name = 'CS455_PROJECT'
+DATA_DIR = '../Animal Dataset/'
+
+image_size = 64
+batch_size = 128
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+
+print(os.listdir(DATA_DIR)[:10])
+print(torch.cuda.is_available())
+
+train_ds = ImageFolder(DATA_DIR, transform=T.Compose([
+    T.Resize(image_size),
+    T.CenterCrop(image_size),
+    T.ToTensor(),
+    T.Normalize(*stats)]))
+
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, pin_memory=True)
+
+show_batch(train_dl)
+
 device = get_default_device()
 print(device)
 
 train_dl = DeviceDataLoader(train_dl, device)
-
-
-import torch.nn as nn
 
 discriminator = nn.Sequential(
     # in: 3 x 64 x 64
@@ -147,127 +236,19 @@ show_images(fake_images)
 
 generator = to_device(generator, device)
 
-def train_discriminator(real_images, opt_d):
-    # Clear discriminator gradients
-    opt_d.zero_grad()
-
-
-    # Pass real images through discriminator
-    real_preds = discriminator(real_images)
-    real_targets = torch.ones(real_images.size(0), 1, device=device)
-    real_loss = F.binary_cross_entropy(real_preds, real_targets)
-    real_score = torch.mean(real_preds).item()
-    
-    # Generate fake images
-    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
-    fake_images = generator(latent)
-
-    # Pass fake images through discriminator
-    fake_targets = torch.zeros(fake_images.size(0), 1, device=device)
-    fake_preds = discriminator(fake_images)
-    fake_loss = F.binary_cross_entropy(fake_preds, fake_targets)
-    fake_score = torch.mean(fake_preds).item()
-
-    # Update discriminator weights
-    loss = real_loss + fake_loss
-    loss.backward()
-    opt_d.step()
-    return loss.item(), real_score, fake_score
-
-
-def train_generator(opt_g):
-    # Clear generator gradients
-    opt_g.zero_grad()
-    
-    # Generate fake images
-    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
-    fake_images = generator(latent)
-    
-    # Try to fool the discriminator
-    preds = discriminator(fake_images)
-    targets = torch.ones(batch_size, 1, device=device)
-    loss = F.binary_cross_entropy(preds, targets)
-    
-    # Update generator weights
-    loss.backward()
-    opt_g.step()
-    
-    return loss.item()
-
-from torchvision.utils import save_image
-
 sample_dir = 'generated'
 os.makedirs(sample_dir, exist_ok=True)
-
-def save_samples(index, latent_tensors, show=True):
-    fake_images = generator(latent_tensors)
-    fake_fname = 'generated-images-{0:0=4d}.png'.format(index)
-    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=8)
-    print('Saving', fake_fname)
-    if show:
-        fig, ax = plt.subplots(figsize=(8, 8))
-        ax.set_xticks([]); ax.set_yticks([])
-        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
 
 fixed_latent = torch.randn(64, latent_size, 1, 1, device=device)
 save_samples(0, fixed_latent)
 
-from tqdm import tqdm
-import torch.nn.functional as F
-
-def fit(epochs, lr, start_idx=1):
-    torch.cuda.empty_cache()
-    
-    # Losses & scores
-    losses_g = []
-    losses_d = []
-    real_scores = []
-    fake_scores = []
-    
-    # Create optimizers
-    opt_d = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
-    opt_g = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
-    
-    for epoch in range(epochs):
-        for real_images, _ in tqdm(train_dl):
-            # Train discriminator
-            loss_d, real_score, fake_score = train_discriminator(real_images, opt_d)
-            # Train generator
-            loss_g = train_generator(opt_g)
-            
-        # Record losses & scores
-        losses_g.append(loss_g)
-        losses_d.append(loss_d)
-        real_scores.append(real_score)
-        fake_scores.append(fake_score)
-        
-        # Log losses & scores (last batch)
-        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
-            epoch+1, epochs, loss_g, loss_d, real_score, fake_score))
-    
-        # Save generated images
-        save_samples(epoch+start_idx, fixed_latent, show=False)
-    
-    return losses_g, losses_d, real_scores, fake_scores
-
-
-
-
 lr = 0.0002
 epochs = 50
-
 
 history = fit(epochs, lr)
 
 losses_g, losses_d, real_scores, fake_scores = history
-torch.save(generator.state_dict(), 'G.ckpt')
-torch.save(discriminator.state_dict(), 'D.ckpt')
-
-
-from IPython.display import Image
-
-
+torch.save(generator.state_dict(), 'generator.ckpt')
+torch.save(discriminator.state_dict(), 'discriminator.ckpt')
 
 Image('./generated/generated-images-0001.png')
-
-
